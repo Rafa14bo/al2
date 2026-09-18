5de5768d650b91f75b2238450023b7e99328c38e
Rafa14bo <jetau2114@gmail.com>
2026-09-18 23:20:15 +0000

    Removendo todos os arquivos

--- README.md
+++
@@ -1,3 +0,0 @@
-# alisson
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-f3evulsy)
--- supabase/migrations/20260815142216_008_revoke_anon_is_admin.sql.sql
+++
@@ -1,2 +0,0 @@
--- Revoke EXECUTE on is_admin from anon role — only authenticated users need it
-REVOKE EXECUTE ON FUNCTION is_admin(uuid) FROM anon;
